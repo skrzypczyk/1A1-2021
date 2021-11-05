bbb513e83b650484cd90ad000185e67c68f06c4c
Yves skrzypczyk <yvesskrzypczyk@macbook-pro-de-yves.home>
2021-11-05 10:52:44 +0100

    boules

--- index.php
+++ index.php
@@ -39,6 +39,7 @@
 		/*
 		Incrémentation et la décrémentation
 		*/
+		/*
 		$cpt = 1;
 		echo $cpt; //1
 		//$cpt+=2 ---> $cpt=$cpt+2
@@ -48,6 +49,7 @@
 		echo $cpt*=3; //12
 		echo $cpt--; //12
 		echo $cpt; //11
+		*/
 
 
 		/*
@@ -69,6 +71,8 @@
 
 		*/
 
+		/*
+
 		if( 0 == null){
 			echo "test";
 		}
@@ -79,7 +83,131 @@
 		elseif( $age==18 )echo "Tout juste majeur";
 		elseif( $age<18 )echo "mineur";
 
+
+		$genre = "Mr";
+
+		switch ($genre) {
+			case 'Mr':
+				echo "Bonjour Monsieur";
+				break;
+
+			case 'Mme':
+				echo "Bonjour Madame";
+				break;
+
+			case 'Mlle':
+				echo "Bonjour Mademoiselle";
+				break;
+
+			default:
+				echo "Bonjour";
+				break;
+		}
+
+		if($genre == "Mr"){
+			echo "Bonjour Monsieur";
+		}elseif ($genre == "Mme"){
+			echo "Bonjour Madame";
+		}elseif ($genre == "Mlle"){
+			echo "Bonjour Mademoiselle";
+		}else{
+			echo "Bonjour";
+		}
+
+
+		// Admin, Editeur, Auteur, simple internaute
+		$scope = "Autre";
 			
+		switch ($scope) {
+			case 'Admin':
+				echo "Peut tout faire";
+
+			case 'Editeur':
+				echo "Il peut publier le contenu";
+			
+			case 'Auteur':
+				echo "Il peut créer le contenu";
+
+			default:
+				echo "Il peut afficher le contenu";
+				break;
+		}
+
+		if($scope == "Admin"){
+			echo "Peut tout faire";
+			echo "Il peut publier le contenu";
+			echo "Il peut créer le contenu";
+			echo "Il peut afficher le contenu";
+
+		}elseif ($scope == "Editeur"){
+			echo "Il peut publier le contenu";
+			echo "Il peut créer le contenu";
+			echo "Il peut afficher le contenu";
+		}elseif ($scope == "Auteur"){
+			echo "Il peut créer le contenu";
+			echo "Il peut afficher le contenu";
+		}else{
+			echo "Il peut afficher le contenu";
+		}
+
+		*/
+
+		/* La condition ternaire */
+		// Juste un if ou un else
+		// avec une unique et meme instruction (echo)
+		// instruction (condition)?vrai:faux;
+
+		$adult = true;
+
+		/*
+			if ($adult == true){
+				echo "Adulte";
+			}else{
+				echo "Mineur";
+			}
+		*/
+
+		//echo ($adult == true)?"Adulte":"Mineur";
+
+
+		/* Le null Coalescent */
+		//Si firstname est null alors il afficher "Anonyme"
+		//echo $firstname??"Anonyme";
+
+
+		$myFirstname = "Yves";
+		// La concaténation "."
+		//echo "Bonjour " . $myFirstname;
+		
+
+
+		// LES BOUCLES
+
+		//à utiliser quand le nb d'itération est inconnu
+		//Symbolise le lancement du dé
+		/*
+		$number = rand(1, 1000);
+		$cpt = 1;
+
+		while ($number != 6) {
+			$number = rand(1, 1000);
+			$cpt++;
+		}
+
+		echo $cpt." tentative(s)";
+		*/
+
+
+		do{
+			...
+		}while (condition);
+		
+		echo $cpt." tentative(s)";
+		
+
+
+
+
 
 
 	?>
